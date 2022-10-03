6e7c24cbe9c5849a07fedfa4b4ce6058a2f03241
LuisGTR26 <luis.gutu2601@gmail.com>
2022-10-03 11:23:16 -0500

    Archivos faltantes

---
+++ app/src/main/java/com/example/prototipo_tea_1/view/fragments/PersonalFragment.kt
@@ -0,0 +1,37 @@
+package com.example.prototipo_tea_1.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import com.example.prototipo_tea_1.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class PersonalFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_personal, container, false)
+
+        //Cuando pulsen regresar
+        val backBtn: Button = view.findViewById(R.id.btnReturn)
+        backBtn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_personalFragment_to_menuFragment)
+        }
+
+        //Cuando pulse el boton para crear una rutina
+        val crearBtn: FloatingActionButton = view.findViewById(R.id.btnAdd)
+        crearBtn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_personalFragment_to_crearRutina)
+        }
+
+        return view
+    }
+
+}
